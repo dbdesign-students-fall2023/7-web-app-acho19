262e349b292913fe75920d7b27a9d5acedeaa264
acho19 <alice.cho@stern.nyu.edu>
2023-11-22 23:31:26 -0500

    first

--- app.py
+++ app.py
@@ -28,6 +28,7 @@ connection = pymongo.MongoClient(config['MONGO_HOST'], 27017,
                                 authSource=config['MONGO_DBNAME'])
 db = connection[config['MONGO_DBNAME']] # store a reference to the database
 
+
 # set up the routes
 
 @app.route('/')
